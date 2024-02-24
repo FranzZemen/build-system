928ddb5e63c8a0ddd88055ca1dda342c5e15c525
Franz Zemen <franzzemen@gmail.com>
2024-02-24 07:42:03 -0500

    pre-version change: update

--- src/project/lib/pipelines/commit.pipeline.ts
+++ src/project/lib/pipelines/commit.pipeline.ts
@@ -7,8 +7,11 @@ License Type: MIT
 import {Pipeline} from "../../pipeline/index.js";
 import {CheckInTransform, CommitTransform} from "../transforms/index.js";
 import {transform} from "lodash";
+import {cleanPipeline} from "./clean.pipeline.js";
+import {buildPipeline} from "./build.pipeline.js";
 
 export const commitPipeline = Pipeline
   .options({name: 'checkInPipeline', logDepth: 0})
+  .append(buildPipeline)
   .transform(CheckInTransform)
   .transform(CommitTransform);
