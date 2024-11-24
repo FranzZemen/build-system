e192663db1ebcf3b7de7087cad49ef72b54f54fb
Franz Zemen <franzzemen@gmail.com>
2024-11-24 14:15:56 -0500

    pre-version change: fix

--- src/project/template/tsconfig.root.ts
+++ src/project/template/tsconfig.root.ts
@@ -3,25 +3,6 @@ Created by Franz Zemen 02/18/2024
 License Type: MIT
 */
 
-/*
-export const tsconfigRoot = {
-  extends: './tsconfig.base.json',
-  compilerOptions: {
-    module: 'NodeNext',
-    target: 'ESNext',
-    moduleResolution: 'NodeNext',
-    tsBuildInfoFile: './out/buildInfo/buildInfo',
-    declarationDir: './out/project/types',
-    rootDir: './src',
-    outDir: './out'
-  },
-
-  */
-  //include: [
-  //  '**/*'
-  //]
-//}
-
 export const tsconfigRoot = {
   "files": [],
   "include": [],
